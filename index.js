c4e3961184c3351c54eeeb22cb9b5497a24afcb7
deepakvenkat <dipak2911@gmail.com>
2013-12-17 21:56:10 -0500

    use the node cron module

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var schedule = require('node-schedule');
+var cronJob = require('cron').CronJob;
 var Bot = require('./modules/bot')
 
 var config = require('./config_files/config')
 var bot = new Bot(config);
-var job = schedule.scheduleJob('*/10 * * * *', function () {
+var job = new cronJob('*/1 * * * *', function () {
   //crontab for every 4 hours
   bot.tweetJob();
 });
+job.start();
