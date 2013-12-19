7d91f520f8db40bc606d8b41580b35dbebc5910c
deepakvenkat <dipak2911@gmail.com>
2013-12-18 22:57:32 -0500

    set the cron job to run every 4 hours

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Bot = require('./modules/bot')
 
 var config = require('./config_files/config')
 var bot = new Bot(config);
-var job = new cronJob('*/1 * * * *', function () {
+var job = new cronJob('* */4 * * *', function () {
   //crontab for every 4 hours
   bot.tweetJob();
 });
